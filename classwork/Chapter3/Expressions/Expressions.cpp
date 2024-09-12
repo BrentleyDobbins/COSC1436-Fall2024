cf65c08b1f853ae804cc5386773d026fe27449aa
Brentley Dobbins <brentley.dobbins@my.tccd.edu>
2024-09-11 20:56:17 -0500

    Chapter 3 Wrap-Up

--- classwork/Chapter3/Expressions/Expressions.cpp
+++ classwork/Chapter3/Expressions/Expressions.cpp
@@ -1,8 +1,12 @@
 
 #include <iostream>
 #include <string>
+#include <cmath>   //C math funtions
+#include <iomanip> //Formatted Output
 
 
+//Include the std namespace so we dont have to prefix it on everything
+using namespace std;
 int main()
 {
     //Get user's name
@@ -71,9 +75,82 @@ int main()
 
 
 
+    // Type-Casting: you convert an expression to another type explicitly
+    // (T)E => C-style Casting, NOT RECOMMENDED
 
+    int totalSales = 50000;
+    int departments = 8;
+
+    // static_cast<T>(E)  => PREFERRED APPROACH/ALWAYS DO IT THIS WAY!
+    //  Must be allowable
+    double averageSalesPerDepartment = static_cast<double>(totalSales) / departments; //Static Cast
+    double averageSalesPerDepatment = totalSales / (double)departments; // C-Style
+
+    //Compiler error, can NOT convert a string to int
+    std::string someString = "Hello";
+    //int someStringNumber = static_cast<int>(someString);
+    //int someStringNumber = (int)someString
+
+    //Use cast to truncated data w/o compiler warning
+    int truncatedValue = static_cast<int>(floatValue);
+
+
+    //Math Functions:
+    // pow(x, y) => x to the y power
+    // sqrt(x) => square root of x
+    // exp(x) => e to the x power
+    // log(x) => log of x log(exp(x)) = x
+    // sin(x), cos(x), tan(x), cot(x), sec(x), csc(x), ...h
+    // abs(x) => positive value of x
+    // ceil(x) => smallest possible int value >= x
+    // floor(x) => largest possible int value <= x
+    // trunc(x) => rounds towards zero
+    // round(x) => rounds to the nearest integer (mid-point rounding)
+
+    double xValue = 45.6;
+    double mathResult;
+
+    std::cout << pow(xValue, 2) << std::endl;   //Same as xValue * xValue
+    std::cout << sqrt(xValue) << std::endl;
+    std::cout << exp(2) << std::endl;           //e nth power, e = 2.718
+    std::cout << log(5.4) << std::endl;         //root of e
+
+    std::cout << abs(-45) << std::endl;         //positive x = 45
+
+    std::cout << ceil(xValue) << std::endl;     // 46
+    std::cout << floor(xValue) << std::endl;    // 45
+
+    std::cout << trunc(xValue) << std::endl;    // 45
+    std::cout << round(xValue) << std::endl;    // 46
+
+
+    //Formatted Output: 
+
+    std::cout << 4.567891241415151 << std::endl;
+    std::cout << 5.67e3 << std::endl;
+
+    //Create a table 
+    // Manipulator     |         Behavior            |     Persist 
+    //   setw(i)       | pads the value to i         | Does NOT persist 
+    //   left          |   Left justifies            | Yes
+    //   right         |   Right justifies           | Yes   (Default)
+    //   fixed         | Forces fixed point notation | Yes
+    // setprecision(i) | Sets precision of floats to total digits if fixed and decimal digits if fixed | Yes
+    //   setfill(c)    |    Character to pad with    | Yes 
+    // //
+
+     
+    cout << std::left << std::setw(8) << "Student" << setw(6) << "Grade " << setw(6) << "Letter " << std::endl;
+    cout << setw(20) << setfill('-') << "" << setfill(' ') << std::endl;
+    cout << fixed << setprecision(2);
+    cout << std::setw(8) << "Bob" << setw(6) << 95.67  << setw(6) << "A" << endl;
+    cout << std::setw(8) << "Sue" << setw(6) << 98.543 << setw(6) << "A" << endl;
+    cout << std::setw(8) << "Jim" << setw(6) << 84.567 << setw(6) << "B" << endl;
+    cout << std::setw(8) << "Jan" << setw(6) << 78.987 << setw(6) << "C" << endl;
 
 
 }
 
 
+
+//Use rectangular selecting for quicker editing
