a98f7dc9ae079edc4ec740b29af8a4210ec0b1ff
Brentley Dobbins <brentley.dobbins@my.tccd.edu>
2024-10-04 14:52:33 -0500

    Lab 2 updated

--- labs/Lab2/LoanPaymentCalculator/LoanPaymentCalculator.cpp
+++ labs/Lab2/LoanPaymentCalculator/LoanPaymentCalculator.cpp
@@ -18,12 +18,31 @@ int main()
 
     int loanAmount;
 
-    cout << "Enter loan amount ($1-$1000): ";
+    cout << "Enter loan amount (1-1000): ";
     cin >> loanAmount;
 
     if (loanAmount < 0 || loanAmount > 1000)
-        cout << "ERROR: Loan amount must be betwwen $1-$1000";
+    {
+        cout << "ERROR: Loan amount must be betwwen 1-1000" << endl;
 
+        cout << "Enter loan amount (1-1000): ";
+        cin >> loanAmount;
+    };
 
+    float interestRate;
+
+    cout << "Enter interest rate (%): ";
+    cin >> interestRate;
+
+    if (interestRate < 1 || interestRate > 100)
+    {
+        cout << "ERROR: Interest rate must be between 1-100" << endl;
+
+        cout << "Enter interest rate (%): ";
+        cin >> interestRate; 
+
+    };
+
+    cout << interestRate / 100;
 
 }
