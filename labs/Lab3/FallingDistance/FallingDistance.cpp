cb262de2ec6994db6a8ef4e9e788454615f90927
Brentley Dobbins <brentley.dobbins@my.tccd.edu>
2024-10-27 14:23:37 -0500

    Updated

--- labs/Lab3/FallingDistance/FallingDistance.cpp
+++ labs/Lab3/FallingDistance/FallingDistance.cpp
@@ -20,4 +20,29 @@ int main()
         cout << "Enter the number of seconds: ";
         cin >> fallingTime;
     }
+
+    char units;
+    cout << "What unit you want the results in meter or feet? (m or f): ";
+    cin >> units;
+    while (units != 'm' && units != 'f')
+    {
+        cout << "ERROR: Unit must m for meter or f for feet " << endl;
+        cout << "What unit you want the results in meter or feet? (m or f): ";
+        cin >> units;
+    }
+
+    cout << "Seconds" << setw(3) << " " << "Distance" << endl;
+    cout << setw(20) << setfill('=') << "" << setfill(' ') << endl;
+
+    double fallingDistance;
+    for (int i = 1; i <= fallingTime; i++)
+    {
+        double g = 9.8;
+        fallingDistance = 0.5 * g * (pow(i, 2));
+        if (units == 'f')
+        {
+            fallingDistance *= 3.28084;
+        }
+        cout << i << setw(9) << " " << fixed << setprecision(1) << fallingDistance << " " << units << endl;
+    };
 }
