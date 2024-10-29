e28d4758dc8b8afc64ff41a8b51334925e0faf2a
Brentley Dobbins <brentley.dobbins@my.tccd.edu>
2024-10-28 21:23:37 -0500

    Notes Updated

--- classwork/Chapter7/Chapter7/Chapter7.cpp
+++ classwork/Chapter7/Chapter7/Chapter7.cpp
@@ -51,9 +51,69 @@ void TestPassByReference()
     Display(rc);
 }
 
+void GradeArrayDemo() //Utilize this for the Lab!!!
+{
+    const int MaxGrades = 20;
+    double dummyValue2 = -1;
+    int grades[MaxGrades];
+    double dummyValue = 0;
+
+    int estimatedMaxArraySize = //Size of array / size of element
+        sizeof(grades) / sizeof(int); //Must be used within the same function/scope; works with local variables but not ideal
+
+    int count = 0;
+    for (int index = 0; index < MaxGrades; ++index)//, ++count)
+    {
+        std::cout << "Enter a grade: ";
+        std::cin >> grades[index];
+        //grades[index] = 100;
+
+        if (grades[index] <= 0)
+            break;
+
+        ++count;
+    };
+
+    //for (int index = 0; index < MaxGrades; ++index)
+    for (int index = 0; index < count; ++index)
+    {
+        std::cout << grades[index] << std::endl;
+    };
+
+    int indexToChange;
+    do
+    {
+        std::cout << "Enter the index of the grade to change: ";
+        std::cin >> indexToChange;
+    } while (indexToChange < 0 || indexToChange >= MaxGrades);
+
+        int newGrade;
+        std::cout << "Enter the new grade: ";
+        std::cin >> newGrade;
+
+        grades[indexToChange] = newGrade;
+
+    
+};
+
+void InitArrayDemo()
+{
+    int MaxRates = 100;
+    double payRates[MaxRates] = {0}; //Zero intialized with = {0}
+
+    //Zero initialize
+    ////for (int index = 0; index < MaxRates; ++index)
+    //////  payRates[index] = 0;
+};
+
 int main()
 {
-    //Array is a setof related data
+    InitArrayDemo();
+}
+
+void NameArrayDemo()
+{
+    //Array is a set of related data
     /*std::string student1;
     std::string student2;
     std::string student3;
